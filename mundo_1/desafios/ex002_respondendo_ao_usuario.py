b310d4fcfc13d2349da06562c8b1da3824d508a0
Mayelly Caires <cairesmayelly@gmail.com>
2026-03-24 17:00:17 -0300

    Atualizando com os ex001 e ex002 da pasta desafios

---
+++ mundo_1/desafios/ex002_respondendo_ao_usuario.py
@@ -0,0 +1,3 @@
+# Crie um programa que pergunte o nome do usuário e depois exibir uma mensagem de boas-vindas personalizada.
+nome = input('Digite seu nome: ')
+print(f'É um prazer te conhecer, {nome}!')
